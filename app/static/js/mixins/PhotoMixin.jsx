8488ebeb8be1a30bfcb5e8cbdc1887b90583fc32
Jesse Patching <jesse@patching.ca>
2015-07-20 17:54:49 -0600

    Fix original url path for lightbox download button

--- app/static/js/mixins/PhotoMixin.jsx
+++ app/static/js/mixins/PhotoMixin.jsx
@@ -7,14 +7,12 @@ define('photo-mixin', ['react'], function(React) {
             // If it's a .gif, just use the original
             if (ext == 'gif' && type == 'display') {
                 ext = 'webm';
-                postfix = '_display';
+                postfix = '_' + type;
+            } else if (type == 'original') {
+                postfix = '';
             } else {
                 ext = 'jpg';
-                if (type == 'original') {
-                    postfix = '';
-                } else {
-                    postfix = '_' + type;
-                }
+                postfix = '_' + type;
             }
 
             return 'https://' + Config.s3_bucket + '.s3.amazonaws.com' + '/' + folder + '/' + name + postfix + '.' + ext;
