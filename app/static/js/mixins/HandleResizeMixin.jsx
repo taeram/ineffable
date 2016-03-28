f8e73be89187c0e05d38975dbac28b5acd4e70be
Jesse Patching <jesse@patching.ca>
2016-03-28 12:14:50 -0600

    Resize once agan after the page is loaded to prevent float drops

--- app/static/js/mixins/HandleResizeMixin.jsx
+++ app/static/js/mixins/HandleResizeMixin.jsx
@@ -6,8 +6,7 @@ define('handle-resize-mixin', ['react', 'underscore'], function(React, _) {
      * Triggered by window resize events
      */
     var HandleResizeMixin = {
-        handleResize: _.debounce(
-            function(e) {
+        handleResize: _.debounce(function(e) {
                 this.setState({windowWidth: window.innerWidth});
             },
             200
@@ -15,6 +14,7 @@ define('handle-resize-mixin', ['react', 'underscore'], function(React, _) {
 
         componentDidMount: function() {
             window.addEventListener('resize', this.handleResize);
+            setTimeout(this.handleResize, 500);
         },
 
         componentWillUnmount: function() {
