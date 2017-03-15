92fcc97b6bbd65b072799df3dfdf1a7400914393
Jesse Patching <jesse@patching.ca>
2017-03-15 11:11:45 -0400

    Fixed Lets Encrypt support

--- app/controllers/helpers/letsencrypt.py
+++ app/controllers/helpers/letsencrypt.py
@@ -2,7 +2,8 @@ from app import app
 from flask import send_from_directory
 import os
 
-@app.route('/.well-known/<path:filename>', methods=['GET'])
+
+@app.route('/.well-known/acme-challenge/<filename>', methods=['GET'])
 def controller_helper_letsencrypt(filename):
     """ Return the letsencrypt file """
-    return send_from_directory(os.path.join(app.root_path, 'static/.well-known'), filename)
+    return send_from_directory(os.path.join(app.root_path, '../.well-known/acme-challenge/'), filename)
